62b868b8be3d53c2eae1f1d1ee355f005bfc8ddc
CodeBlock177 <codeblock177@gmail.com>
2023-03-18 01:01:52 -0600

    empezando hacer menu hamburgesa para celulares

--- js/main.js
+++ js/main.js
@@ -1,25 +1,18 @@
-const typed = new Typed('.typed', {
-    /*
-    strings: 
-    [
-        'Eduardo', 
-        'Williams',
-        'Desarollador Web'
-    ],
-    */
+const typed = new Typed('.typed', 
+{
+    stringsElement: "#string",
+    typeSpeed: 75,
+    startDeley: 300,
+    backSpeed: 75,
+    smartBackspace: true,
+    backDelay: 1500,
+    showCursor: true,
+});
 
-    stringsElement: "#string", //Id del elemento que contiene cadenas de texto a mostrar.
-    typeSpeed: 75, // velocidad en milisegundos para poner una letra.
-    startDeley: 300, // tiempo de retraso en iniciar la animacion cuando carga la pagina. aplica tambien cuando termina y vuelve a iniciar
-    backSpeed: 75, // velocidad en milisegundos para borrar una letra
-    smartBackspace: true, // eliminar solamente las palabras que sean nuevas en la cadena de texto.
-    shuffle: false, // alterar el orden en el que se escriben las palabras.
-    backDelay: 1500, // tiempo de espera despues de que termina de escribir una palabra
-    loop: false, // tepetir el array de string.
-    loopCount: false, // cantidad de veces a repetir el array. false = infinite.
-    showCursor: true, // mostrar cursor palpitando.
-    cursorChar: '|', // caracter para el cursor
-    contentType: 'html', // 'html' o 'null' para texto sin formato
+const button = document.querySelector(".theme");
 
-
+button.addEventListener('click', () => 
+{
+    document.body.classList.toggle('dark');
+    button.classList.toggle('active');
 });
